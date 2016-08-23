d87fc18adc3bf65cf80c87ee7978971ec623e2eb
Kaleb Clark <kaleb@abraxxus.net>
2016-08-22 21:45:53 -0700

    Fixed typo in cheat sheet init

--- smash.sh
+++ smash.sh
@@ -7,6 +7,6 @@ load_smash_dir ~/.smash/git         # Load GIT Custom settings
 load_smash_dir ~/.smash/ssh         # Load SSH Custom settings
 load_smash_dir ~/.smash/www         # Load WWW Custom Settings
 
-source ~/.smash/cheatsheets/cheats.sh # Load Cheat Sheet function
+source ~/.smash/cheat_sheets/cheats.sh # Load Cheat Sheet function
 
 export EDITOR=$SMASH_EDITOR
